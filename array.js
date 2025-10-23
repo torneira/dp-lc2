696fe02555fc89565a21e1fb0af0099d9e6e0711
Gustavo <118356161+torneira@users.noreply.github.com>
2025-10-23 08:31:44 -0400

    Update array.js

--- array.js
+++ array.js
@@ -1,12 +1,62 @@
-let arr = []
+// let arr = []
 
-for(let i = 0; i < 8; i++){
-    arr.push(Math.floor(Math.random() * 100) + 1)
+// for(let i = 0; i < 8; i++){
+//     arr.push(Math.floor(Math.random() * 100) + 1)
+// }
+
+// console.log(arr)
+
+// let num1 = arr[Math.floor(Math.random() * 8) + 1]
+// let num2 = arr[Math.floor(Math.random() * 8) + 1]
+
+// soma = console.log("A soma de " + num1 + " + " + num2 + " é igual a: ", num1 + num2)
+
+
+//1
+
+// let m = [
+//     [0,0,0,0],
+//     [0,0,0,0],
+//     [0,0,0,0],
+//     [0,0,0,0]
+// ]
+
+// console.table(m)
+
+//2
+
+// let m = [
+//     [],
+//     [],
+//     []
+// ]
+
+// for(let i = 0; i < 3; i++){
+//     for(let j = 0; j < 4; j++){
+//         m[i][j] = Math.floor(Math.random() * 10)
+//     }
+//     console.table(m)
+// }
+
+// console.table(m)
+
+//3
+
+let m = [
+    [],
+    [],
+    [],
+    []
+]
+
+let soma = 0
+
+for(let i = 0; i < 4; i++){
+    for(let j = 0; j < 4; j++){
+        m[i][j] = Math.floor(Math.random() * 10)
+        soma = soma+m[i][j]
+    }
+    console.table(m)
+    console.log(soma)
+
 }
-
-console.log(arr)
-
-let num1 = arr[Math.floor(Math.random() * 8) + 1]
-let num2 = arr[Math.floor(Math.random() * 8) + 1]
-
-soma = console.log("A soma de " + num1 + " + " + num2 + " é igual a: ", num1 + num2)
