b226441bbfe0b96fa3c5c3bab92340a74851145e
Gustavo <118356161+torneira@users.noreply.github.com>
2025-11-06 08:22:40 -0400

    Create funcoes-nativas.js

---
+++ funcoes-nativas.js
@@ -0,0 +1,21 @@
+// let frutas = []
+
+// frutas.push("maçã", "banana", "laranja")
+// frutas.unshift("uva", "manga")
+
+// console.log(frutas)
+
+// frutas.pop()
+// frutas.shift()
+
+// console.log(frutas)
+
+let numeros = [4,10,2,8,6]
+
+numeros.sort(function(a,b){ return a-b; })
+
+console.log(numeros)
+
+numeros.reverse()
+
+console.log(numeros)
