5516584a0d171f5fb8c3c0a964ce0230d3a2b462
Gustavo <118356161+torneira@users.noreply.github.com>
2025-12-04 07:21:12 -0400

    Create prova-p2.js

---
+++ prova-p2.js
@@ -0,0 +1,9 @@
+const PromptSync = require("prompt-sync");
+
+const prompt = require(PromptSync)
+let alunos = [
+  ["Ana", 2021, 7.5, 8.0],
+  ["Bruno", 2022, 5.0, 6.0],
+  ["Carla", 2023, 9.0, 8.5]
+];
+
